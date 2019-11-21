b46c7d5d377065d574457c9548057063e0c227a3
Wang <yao.h.wang@accenture.com>
2019-11-21 22:12:33 +0000

    update

--- js/src/util/aside.js
+++ js/src/util/aside.js
@@ -1,5 +1,5 @@
-var post_aside = document.querySelector("link[rel = 'import_aside']").import;
-var con_aside = post_aside.querySelector("div");
+var aside = document.querySelector("link[rel = 'import_aside']").import;
+var con_aside = aside.querySelector("div");
 document.querySelector("#sidebar").appendChild(con_aside.cloneNode(true));
 
 var clone_aside = document.importNode(con_aside,true)
