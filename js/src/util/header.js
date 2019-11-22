33e5d8fead7732871ae141cd13d5921c6bc11e39
Wang <yao.h.wang@accenture.com>
2019-11-22 22:10:30 +0000

    update

--- js/src/util/header.js
+++
@@ -1,6 +0,0 @@
-var post = document.querySelector("link[rel = 'import']").import;
-var con = post.querySelector("div");
-document.querySelector("#header").appendChild(con.cloneNode(true));
-
-var clone = document.importNode(con,true)
-document.querySelector("#header").appendChild(clone)
